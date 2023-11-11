2a04e8a900fb96bed166a750e9b89a7dd7cdffa8
baitemir <baitemir1706@gmail.com>
2023-11-11 21:38:49 +0600

    updated permissions

--- router/dish-router.js
+++ router/dish-router.js
@@ -1,13 +1,14 @@
 const Router = require("express").Router;
 const dishController = require("../controller/dish-controller");
 const AuthMiddleware = require("../middleware/auth-middleware");
+const adminMiddleware = require("../middleware/admin-middleware");
 
 const router = new Router();
 
 router.get("/all", AuthMiddleware, dishController.getAllDishes);
 router.get("/:id", AuthMiddleware, dishController.getDish);
-router.post("/create", AuthMiddleware, dishController.createDishes);
-router.put("/update/:id", AuthMiddleware, dishController.updateDish);
-router.delete("/delete/:id", AuthMiddleware, dishController.deleteDish);
+router.post("/create", adminMiddleware, dishController.createDishes);
+router.put("/update/:id", adminMiddleware, dishController.updateDish);
+router.delete("/delete/:id", adminMiddleware, dishController.deleteDish);
 
 module.exports = router;
